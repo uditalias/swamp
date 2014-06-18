7ca5eff90f48659fda715d95386171b75e91a1be
udidu <udi.talias@gmail.com>
2014-06-18 23:31:01 +0300

    ioStream controller now support loading log files list by type (err or out)

--- config/routes/ioStream/root.js
+++ config/routes/ioStream/root.js
@@ -4,4 +4,14 @@ module.exports = [{
     method: 'get',
     controller: 'ioStream',
     action: 'get'
+}, {
+    path: '/io/:serviceId/:type/stream/',
+    method: 'get',
+    controller: 'ioStream',
+    action: 'getStream'
+}, {
+    path: '/io/:serviceId/:type/list/',
+    method: 'get',
+    controller: 'ioStream',
+    action: 'getLogFilesList'
 }];
