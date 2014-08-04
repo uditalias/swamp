3eb98edb9d091515213c3f04d7398ccca578bdd7
alonisser <alonisser@gmail.com>
2014-08-05 00:32:18 +0300

    Progress with installation script

--- utils/bootstrap.sh
+++ utils/bootstrap.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
-cp ./swamp.conf /etc/init/swamp.conf
+INSTALL_PATH=~/.swamp
+read -p "Swampfile installation path? (Defaults for $INSTALL_PATH)"
+mkdir -pv $INSTALL_PATH
+
+RUN_USER=$(ld -u -n)
+read -p "User to run swamp as (defaults to $RUN_USER)"
+
+RUN_GROUP=$(ld -g -n)
+read -p "Group to run swamp as (defaults to $RUN_GROUP)"
+
+#check for upstart which initctl
+cp ./initscripts/upstart/swamp.conf /tmp/swamp.conf
+
+sed -i s/username/$RUN_USER/ /tmp/swamp.conf
+sed -i s/groupname/$RUN_GROUP/ /tmp/swamp.conf
+
+cp /tmp/swamp.conf /etc/init/swamp.conf
+
