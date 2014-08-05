7d96d4c7ab6f7df8b86aca828d1a4b3a5f6b9d47
alonisser <alonisser@gmail.com>
2014-08-05 11:46:13 +0300

    Progress

--- utils/bootstrap.sh
+++ utils/bootstrap.sh
@@ -1,19 +1,40 @@
 #!/bin/sh
+
+set -e
 INSTALL_PATH=~/.swamp
-read -p "Swampfile installation path? (Defaults for $INSTALL_PATH)"
+read -p "Swampfile installation path? (Defaults for $INSTALL_PATH)" -r REPLY
+echo  # move line
+
+if [ -n "$REPLY" ]; then
+  INSTALL_PATH="$REPLY"
+fi
+echo $INSTALL_PATH
 mkdir -pv $INSTALL_PATH
 
-RUN_USER=$(ld -u -n)
-read -p "User to run swamp as (defaults to $RUN_USER)"
+RUN_USER=$(id -u -n)
+read -p "User to run swamp as (defaults to $RUN_USER)" -r REPLY
 
-RUN_GROUP=$(ld -g -n)
-read -p "Group to run swamp as (defaults to $RUN_GROUP)"
+if [ -n "$REPLY" ]; then
+  RUN_USER="$REPLY"
+fi
+
+
+RUN_GROUP=$(id -g -n)
+read -p "Group to run swamp as (defaults to $RUN_GROUP)" -r REPLY
+
+if [ -n "$REPLY" ]; then
+  RUN_GROUP="$REPLY"
+fi
+
 
 #check for upstart which initctl
-cp ./initscripts/upstart/swamp.conf /tmp/swamp.conf
+if which initctl; then
+  cp ./initscripts/upstart/swamp.conf /tmp/swamp.conf
 
-sed -i s/username/$RUN_USER/ /tmp/swamp.conf
-sed -i s/groupname/$RUN_GROUP/ /tmp/swamp.conf
+  sed -i s/username/$RUN_USER/ /tmp/swamp.conf
+  sed -i s/groupname/$RUN_GROUP/ /tmp/swamp.conf
 
-cp /tmp/swamp.conf /etc/init/swamp.conf
+  cp /tmp/swamp.conf /etc/init/swamp.conf
+
+fi
 
